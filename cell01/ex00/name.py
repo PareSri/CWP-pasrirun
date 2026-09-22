1fabd7a12fb05b858a16a62dc4760e70c73c18a5
แพร ศรีหรั่งไพโรจน์ <pare@Macintosh-5.local>
2026-09-22 12:41:00 +0700

    add all cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,11 @@
+""" name.py """
+
+
+def main():
+    """ name.py """
+    first_name = 'Pare'
+    last_name = 'Srirungpairoj'
+    print(f'{first_name} {last_name}')
+
+
+main()
---
+++ cell01/ex01/name.py
@@ -0,0 +1,12 @@
+""" name.py """
+
+
+def main():
+    """ name.py """
+    first_name = 'Pare'
+    last_name = 'Srirungpairoj'
+    whole_name = first_name + ' ' + last_name
+    print(f'{whole_name}')
+
+
+main()
